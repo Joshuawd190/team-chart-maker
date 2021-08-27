ae4e172989132bf03fcc0de294782146aeb8f0b0
Joshua D Wood <joshuawd190@gmail.com>
2021-08-26 23:42:17 -0500

    Set up template and css

--- src/template.js
+++ src/template.js
@@ -1,8 +1,19 @@
-module.exports = function templateData(employees) {
-  employees.map((data) => {
-    //for each employee, create a card, then a switch case for each type to fill in the differences
-  });
 
+function generateCard(name,email,id,role,roleInfo){
+    return`
+        <div>
+        <h2>${name}</h2>
+        <h3>${role}</h3>
+        <ul>
+        <li>ID: ${id}</li>
+        <li>Email: <a href="mailto: ${email}">${email}</a></li>
+        <li>${roleInfo}</li>
+        </ul>
+        </div>
+    `
+}
+
+module.exports = function templateData(employees) {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,19 +21,38 @@ module.exports = function templateData(employees) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Team</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./index.css">
 </head>
 <body>
-    
+<header>
+<h1>My Team</h1>
+</header>
+ <section>   
 ${employees.map((data) => {
   //for each employee, create a card, then a switch case for each type to fill in the differences
   const name = data.getName();
-  const type = data.name;
   const email = data.getEmail();
+  const id = data.getID();
+  const role = data.getRole();
+  const roleInfo;
+  switch (role) {
+    case 'Manager':
+        roleInfo = "Office number: " + data.getOfficeNum();
+        break;
+    case 'Engineer':
+        roleInfo = `GitHub: <a href="https://github.com/${data.getGithub()}">${data.getGithub()}</a>`;
+        break;
+    case 'Intern':
+        roleInfo = "School Name: " + data.getSchool();
+        break;
+    default:
+        break;
+  }
+
+  generateCard(name, email, id, role, roleInfo);
 })}
-    
+</section>    
 </body>
 </html>
-
 `;
 };
